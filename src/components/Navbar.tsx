c725a44151e8d9b63c23b226eb3deefd13f626dc
sansil <sansil32@gmail.com>
2022-08-16 16:13:20 -0300

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+/* This example requires Tailwind CSS v2.0+ */
+const navigation = [
+  { name: 'Solutions', href: '#' },
+  { name: 'Pricing', href: '#' },
+  { name: 'Docs', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+import ConnectWallet from '@/components/ConnectWallet'
+import toninasLogo from "@/assets/toninas.png"
+export default function NavBar() {
+  return (
+    <header className="w-full bg-indigo-600">
+      <nav className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8" aria-label="Top">
+        <div className="flex items-center justify-between w-full py-6 border-b border-indigo-500 lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="w-auto h-10 rounded-full"
+                src="../../public/toninas.png"
+                alt=""
+              />
+            </a>
+            <div className="hidden ml-10 space-x-8 lg:block">
+              {navigation.map((link) => (
+                <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+                  {link.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="ml-10 space-x-4">
+            <ConnectWallet />
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center py-4 space-x-6 lg:hidden">
+          {navigation.map((link) => (
+            <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
